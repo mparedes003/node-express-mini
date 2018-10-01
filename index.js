f42c16bca59d5b1294a9c4b12399870cafe639fb
mparedes003 <michelle.g.paredes@gmail.com>
2018-10-01 11:21:16 -0700

    Used server.get() to get the users information.

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 // implement your API here
-const express = require('express');
+// introduce how routing works
 
-const server = express();
+// import express from 'express'; // ES2015 moduels . export default someCode;
+const express = require('express'); // CommonJS Modules > modules.exports = someCode;
+const cors = require('cors') // install this package to connect from react
 
+const db = require('./data/db.js');
+
+const server = express(); // creates the server
+
+server.use(cors()); // this needed to connect from react
+
+server.get('/', (req, res) => {
+  // request/route handler
+  res.send('<h1>Hello FSW13!</h1>');
+});
+
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => {
+      console.log('\n** usrers **', users);
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+// watch for traffic in a particular computer port
 const port = 8000;
 server.listen(port, () =>
-  console.log(`\n=== API running on port ${port} ===\n`));
+  console.log(`\n=== API running on port ${port} ===\n`)
+  );
+
+// http://localhost:3000 > the 3000 is the port.
+// 80: http, 443: https, 25: email servers
+// npm run server or yarn server in our case
